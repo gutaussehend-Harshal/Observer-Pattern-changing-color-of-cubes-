b1fc432a47a1cdfdd49b6efba09168e1db2b0788
Harshal Amrut Patil <harshal.patil@gmail.com>
2021-09-11 23:56:29 +0530

    Implemented Observer Pattern.

---
+++ Observer Pattern/Assets/Scripts/Cube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Material _mesh;
+
+    private void OnEnable()
+    {
+        _mesh = GetComponent<MeshRenderer>().material;
+        PlayerObserver.onTurnRed += Player_onTurnRed;
+    }
+
+    private void Player_onTurnRed()
+    {
+        _mesh.color = new Color(Random.value, Random.value, Random.value);
+    }
+
+    private void OnDisable()
+    {
+        PlayerObserver.onTurnRed -= Player_onTurnRed;
+    }
+}
---
+++ Observer Pattern/Assets/Scripts/PlayerObserver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerObserver : MonoBehaviour
+{
+    public static event Action onTurnRed;
+
+    void Update()
+    {
+        ChangeColor();
+    }
+
+    private void ChangeColor()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            onTurnRed?.Invoke();
+        }
+    }
+}
